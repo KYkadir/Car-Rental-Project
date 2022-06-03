e95fd4e47f7005161e6bf5fa779695915cf2d931
KYkadir <101402050+KYkadir@users.noreply.github.com>
2022-06-03 13:37:27 +0200

    ilk

---
+++ Runner.java
@@ -0,0 +1,15 @@
+package ArabaKiralamaProjesi;
+
+public class Runner {
+    public static void main(String[] args) throws InterruptedException {
+
+        ArabaVeMüsteriBilgisi start = new ArabaVeMüsteriBilgisi();
+        start.aracTalepEt();
+        start.arabaÖzellikleri();
+        start.müsteriBilgisi();
+        start.ödemeBilgileri();
+
+
+    }
+
+}
